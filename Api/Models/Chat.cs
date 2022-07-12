bda63c180abb09062ea09e882663bfa3add31e6a
isnakolah <nakolahdaniel1@gmail.com>
2022-07-12 16:51:35 +0300

    Use timestamp as hashkey

--- Api/Models/Chat.cs
+++ Api/Models/Chat.cs
@@ -1,21 +1,24 @@
-﻿using Amazon.DynamoDBv2.DataModel;
+﻿using System.Text.Json.Serialization;
+using Amazon.DynamoDBv2.DataModel;
+using Api.Common;
 
 namespace Api.Models;
 
 [DynamoDBTable("chat-api-table")]
 public record Chat
 {
-    public Chat()
-    {
-    }
-    
-    public Chat(string message, string session, string user)
-    {
-        (Message, Session, User) = (message, session, user);
-    }
+    [JsonIgnore]
+    [DynamoDBHashKey("timestamp")]
+    public long Timestamp { get; init; } = DateTime.Now.DateTimeToUnixTimestamp();
 
-    [DynamoDBHashKey("created-on")] public DateTime CreatedOn { get; init; } = DateTime.Now;
-    [DynamoDBProperty("message")] public string Message { get; init; } = default!;
-    [DynamoDBProperty("session")] public string Session { get; init; } = default!;
-    [DynamoDBProperty("user")] public string User { get; init; } = default!;
+    [DynamoDBProperty("message")]
+    public string Message { get; init; } = default!;
+
+    [DynamoDBProperty("session")]
+    public string Session { get; init; } = default!;
+
+    [DynamoDBProperty("user")]
+    public string User { get; init; } = default!;
+
+    public DateTime CreatedOn => Timestamp.UnixTimeStampToDateTime();
 }
