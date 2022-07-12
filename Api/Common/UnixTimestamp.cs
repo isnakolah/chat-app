c1e2d74d6b1b15fc81c3e583ebd0ca2109db5c0b
isnakolah <nakolahdaniel1@gmail.com>
2022-07-12 16:50:52 +0300

    Create an extension method to convert to timestamp and back

---
+++ Api/Common/UnixTimestamp.cs
@@ -0,0 +1,20 @@
+﻿namespace Api.Common;
+
+internal static class Timestamp
+{
+    public static long DateTimeToUnixTimestamp(this DateTime dateTime)
+    {
+        var timestamp = new DateTimeOffset(dateTime).ToUnixTimeSeconds();
+
+        return timestamp;
+    }
+
+    public static DateTime UnixTimeStampToDateTime(this long unixTimestamp)
+    {
+        var nineteenSeventy = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        var convertedTime = nineteenSeventy.AddSeconds(unixTimestamp).ToLocalTime();
+
+        return convertedTime;
+    }
+}
